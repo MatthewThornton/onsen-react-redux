f0bdd70850ee2b4a19a520683ea8713eb9d00d49
Matthew Thornton <matthewthornton.usa@gmail.com>
2019-01-23 13:20:19 -0800

    add redux store and simple change input form

--- containers/App.js
+++ containers/App.js
@@ -1,7 +1,22 @@
-import React, { Component } from 'react';
-
-export default class App extends Component {
+import React, { Component } from "react";
+import InputPreview from "../components/InputPreview";
+import { connect } from "react-redux";
+import { setMessage } from "../actions/message";
+class App extends Component {
+    _onChange = (value) => {
+        this.props.dispatch(setMessage(value))
+    }
     render() {
-        return <p>This is my new react app</p>
+        const { message } = this.props.messageReducer;
+        return (
+            <div>
+                <p>{ message } </p>
+                <InputPreview
+                    value={message}
+                    onChange={this._onChange} 
+                />
+            </div>
+        )
     }
 }
+export default connect(state => state)(App);
