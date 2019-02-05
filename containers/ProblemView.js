bd9eb3e14500bd381e5316612a667de9406e9727
Matthew Thornton <matthewthornton.usa@gmail.com>
2019-02-05 11:52:37 -0800

    webpack babel updates

--- containers/ProblemView.js
+++ containers/ProblemView.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ReactHtmlParser, { processNodes, convertNodeToElement, htmlparser2 } from 'react-html-parser';
 import Canvas from "../components/Canvas";
 import OneStep from "../problem_vault/one_step_equation/Presentation"
 
--- webpack/webpack.dev.config.js
+++ webpack/webpack.dev.config.js
@@ -4,26 +4,29 @@ var path = require('path');
 var parentDir = path.join(__dirname, '../');
 
 module.exports = {
-    entry: [
-        path.join(parentDir, 'index.js')
-    ],
+    entry: [path.join(parentDir, "index.js")],
+    mode: "development",
     module: {
-        loaders: [{
-            test: /\.(js|jsx)$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader'
-        }, {
-            test: /\.less$/,
-            loaders: ["style-loader", "css-loader", "less-loader"]
-        }
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            }
         ]
     },
-    output: {
-        path: parentDir + '/dist',
-        filename: 'bundle.js'
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
     },
+    output: {
+        path: parentDir + "/dist",
+        filename: "bundle.js"
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
     devServer: {
         contentBase: parentDir,
-        historyApiFallback: true
+        hot: true
     }
-}
+};
